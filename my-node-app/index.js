ad1db2864a9bc5eae60cef7bb8b19c9bef162b75
ahtesham-haq <ahtesham.ul.haq@healthwaresystems.com>
2024-08-29 16:09:55 +0500

    Update index.js

--- my-node-app/index.js
+++ my-node-app/index.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`a running at http://${hostname}:${port}/`);
+  console.log(`ccccccccccccccccca running at http://${hostname}:${port}/`);
 });
